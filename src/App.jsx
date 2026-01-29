8537dc18e1239fa5d0e8e5ee6053b8b55eee0ca8
Samantha G <antashma@gmail.com>
2026-01-29 17:13:56 -0500

    fixed mobile styling hopefully

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 			<h1>Focus Time: Sessions</h1>
 			<p>A simple 25/5 pomodoro timer.</p>
 			<p>Created with 💖 by <a href="http://www.antashma.dev" target='_blank'>Antashma</a></p>
-			<p>Starfield Background by <a href="https://dev.to/usman_awan/how-i-built-a-grok-inspired-starfield-shooting-stars-using-html-canvas-3872">Usman Awan</a></p>
+			<p>Starfield Background by <a href="https://dev.to/usman_awan/how-i-built-a-grok-inspired-starfield-shooting-stars-using-html-canvas-3872" arget='_blank'>Usman Awan</a></p>
 		</footer>
 		</>
 	)
